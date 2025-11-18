c0ce9dfeb6446e100b1fcc1678579c9555a589d6
ReMiguelJr <r_migueel@hotmail.com>
2025-11-18 14:23:46 -0300

    Add SQL tables for pessoa_fisica, aluno, professor, and fornecedor

--- script.sql
+++ script.sql
@@ -5,6 +5,7 @@ CREATE TABLE pessoa (
   cep VARCHAR(8) NOT NULL
 );
 
+
 CREATE TABLE pessoa_fisica (
   id_pessoa BIGINT PRIMARY KEY,
   cpf VARCHAR(11) UNIQUE NOT NULL,
@@ -13,12 +14,14 @@ CREATE TABLE pessoa_fisica (
     FOREIGN KEY (id_pessoa) REFERENCES pessoa(id)
 );
 
+
 CREATE TABLE aluno (
   id_pessoa BIGINT PRIMARY KEY,
   matricula VARCHAR(150) UNIQUE NOT NULL,
   CONSTRAINT fk_aluno_pf
     FOREIGN KEY (id_pessoa) REFERENCES pessoa_fisica(id_pessoa)
 );
+
 
 CREATE TABLE professor (
   id_pessoa BIGINT PRIMARY KEY,
@@ -27,12 +30,14 @@ CREATE TABLE professor (
     FOREIGN KEY (id_pessoa) REFERENCES pessoa_fisica(id_pessoa)
 );
 
+
 CREATE TABLE pessoa_juridica (
   id_pessoa BIGINT PRIMARY KEY,
   cnpj VARCHAR(18) UNIQUE NOT NULL,
   CONSTRAINT fk_pj_pessoa
     FOREIGN KEY (id_pessoa) REFERENCES pessoa(id)
 );
+
 
 CREATE TABLE fornecedor (
   id_pessoa BIGINT PRIMARY KEY,
